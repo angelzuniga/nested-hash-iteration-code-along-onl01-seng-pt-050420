f29adc7f999a2045ecc5aa0e21ac71e873275470
Angel Zuniga <angelzuniga@outlook.com>
2020-05-18 01:42:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,6 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
@@ -27,3 +24,4 @@ def remove_strawberry(contacts)
       end
     end
   end
+end
